aa396c63c4f7302c0712cac246c962b869713a33
mikaeljohnson <mikaeljohnsoncrypto@gmail.com>
2021-11-03 00:34:38 -0400

    attempt at foxing gif display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-light fixed-top bg-light flex-md-nowrap p-1 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="https://rinkeby.etherscan.io/address/0xc3e1ab72e98b13d625ef2b7eb98e7aa708dfc90c#code"
+          href="https://rinkeby.etherscan.io/address/0x3EF59268579816F3D650F29bFfb57710739e2947"
           target="_blank"
           rel="noopener noreferrer"
         >
